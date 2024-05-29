2478c1f7d3df26758fc21b786dbe745372ae96d9
jowonyoung1 <jwy7254@naver.com>
2024-05-29 16:59:06 +0900

    feat : header open menu 반응형 작업중

--- public/www/js/headeropen.js
+++ public/www/js/headeropen.js
@@ -5,30 +5,44 @@ window.addEventListener("load", function () {
 
   let nvMenuOpen = false;
 
-  // navi-menu 클릭시 header-open-menu 보여짐
+  // navi-menu 클릭 시 header-open-menu 보여짐
   headerOpenBt.addEventListener("click", function (event) {
     event.preventDefault();
     console.log("클릭");
 
-    if (nvMenuOpen === false) {
-      // headerOpenBt.classList.remove("navi-menu-open");
-      // headerOpenBg.classList.remove("header-open-bg-show");
-      // headerOpenMu.classList.remove("header-open-menu-show");
-      // nvMenuOpen = false;
-      headerOpenBt.classList.add("navi-menu-open");
-      headerOpenBg.classList.add("header-open-bg-show");
-      headerOpenMu.classList.add("header-open-menu-show");
+    if (!nvMenuOpen) {
+      headerOpenBg.style.display = "block"; // 배경 표시
+      headerOpenMu.style.display = "block"; // 메뉴 표시
+      setTimeout(() => {
+        headerOpenBt.classList.add("navi-menu-open");
+        headerOpenBg.classList.add("header-open-bg-show");
+        headerOpenMu.classList.add("header-open-menu-show");
+      }, 10); // 트랜지션을 적용하기 위한 약간의 딜레이
       nvMenuOpen = true;
-    }
-
-    // 빈 화면 클릭시 header-open-menu 닫혀짐
-    const hdBgClose = document.querySelector(".header-open-bg");
-    hdBgClose.addEventListener("click", function (event) {
-      event.preventDefault();
-      console.log("닫힘");
+    } else {
+      headerOpenBt.classList.remove("navi-menu-open");
       headerOpenBg.classList.remove("header-open-bg-show");
       headerOpenMu.classList.remove("header-open-menu-show");
+      setTimeout(() => {
+        headerOpenBg.style.display = "none"; // 트랜지션 후 배경 숨기기
+        headerOpenMu.style.display = "none"; // 트랜지션 후 메뉴 숨기기
+      }, 300); // 트랜지션 시간과 동일한 딜레이
       nvMenuOpen = false;
-    });
+    }
+  });
+
+  // 빈 화면 클릭 시 header-open-menu 닫힘
+  headerOpenBg.addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log("닫힘");
+
+    headerOpenBt.classList.remove("navi-menu-open");
+    headerOpenBg.classList.remove("header-open-bg-show");
+    headerOpenMu.classList.remove("header-open-menu-show");
+    setTimeout(() => {
+      headerOpenBg.style.display = "none"; // 트랜지션 후 배경 숨기기
+      headerOpenMu.style.display = "none"; // 트랜지션 후 메뉴 숨기기
+    }, 300); // 트랜지션 시간과 동일한 딜레이
+    nvMenuOpen = false;
   });
 });
